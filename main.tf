b3a02942d5a62479511bf198aadedfb270f7cee6
prathjan <prathjan@cisco.com>
2022-04-14 20:18:41 -0700

    old ver

--- main.tf
+++ main.tf
@@ -16,25 +16,25 @@ variable "ikswsname" {
   type = string
 }
 
-#resource helm_release helloiksfrtfcb {
-#  name       = "helloiksapp"
-#  namespace = "default"
-#  chart = "https://prathjan.github.io/helm-chart/helloiks-0.1.1.tgz"
+resource helm_release helloiksfrtfcb {
+  name       = "helloiksapp"
+  namespace = "default"
+  chart = "https://prathjan.github.io/helm-chart/helloiks-0.1.1.tgz"
 
-#  set {
-#    name  = "MESSAGE"
-#    value = "Hello IKS from TFCB!!"
-#  }
-#}
+  set {
+    name  = "MESSAGE"
+    value = "Hello IKS from TFCB!!"
+  }
+}
 
-#provider "helm" {
-#  kubernetes {
-#    host = local.kube_config.clusters[0].cluster.server
-#    client_certificate = base64decode(local.kube_config.users[0].user.client-certificate-data)
-#    client_key = base64decode(local.kube_config.users[0].user.client-key-data)
-#    cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
-#  }
-#}
+provider "helm" {
+  kubernetes {
+    host = local.kube_config.clusters[0].cluster.server
+    client_certificate = base64decode(local.kube_config.users[0].user.client-certificate-data)
+    client_key = base64decode(local.kube_config.users[0].user.client-key-data)
+    cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
+  }
+}
 
 output "kube_config" {
   value = base64decode(local.kube_config)
